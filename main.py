abb65c5cc8c3cdf897f34757a58e40ced79347cf
cheng guo <c43guo@uwaterloo.ca>
2016-05-08 23:00:23 -0400

    update

---
+++ main.py
@@ -0,0 +1,13 @@
+import threading
+from queue import Queue
+from general import *
+from domain import *
+from spider import Spider
+
+
+PROJECT_NAME = 'thenewboston'
+HOMEPAGE = 'https://thenewboston.com/'
+DOMAIN_NAME = get_domain_name(HOMEPAGE)
+QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+NUMBER_OF_THREADS = 
