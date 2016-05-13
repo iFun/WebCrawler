49fa6843f92965eed8e73a898fb9be58134a9a4a
cheng guo <c43guo@uwaterloo.ca>
2016-05-12 22:05:09 -0400

    bug fix

--- spider.py
+++ spider.py
@@ -49,19 +49,22 @@ class Spider:
     @staticmethod
     def gather_link(page_url):
         html_string = ''
+        finder = ''
         # goto website, get the byte data convert to string
         # pass it through to linkfinder, and find all the links
         html_string = ''
         try:
             response = urlopen(page_url)
-            if 'text/html' in response.get_header('Content-Type'):
+            if 'text/html' in response.info().headers:
                 html_bytes = response.read()
                 html_string = html_bytes.decode("utf-8")
-            finder = LinkFinder(Spider.base_url, page_url)
-            finder.feed(html_string)
+                finder = LinkFinder(Spider.base_url, page_url)
+                finder.feed(html_string)
+           
         except Exception as e:
             print(str(e))
             return set()
+
         return finder.page_links()
 
 
